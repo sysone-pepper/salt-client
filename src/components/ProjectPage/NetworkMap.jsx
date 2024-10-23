6a72e4c4076ff7ff0df990dd77a1ed6cbc5ae6e1
cookooyas <cookooyas@gmail.com>
2024-10-23 18:14:47 +0900

    feature: 구성도 내 링크 정보 반영, 링크 추가 기능 구현

--- src/components/ProjectPage/NetworkMap.jsx
+++ src/components/ProjectPage/NetworkMap.jsx
@@ -2,7 +2,9 @@ import ReactDOMServer from "react-dom/server";
 import cytoscape from "cytoscape";
 import dagre from "cytoscape-dagre";
 import nodeHtmlLabel from "cytoscape-node-html-label";
-import { useContext, useEffect, useRef } from "react";
+import edgehandles from "cytoscape-edgehandles";
+import EHoptions from "../../constants/EdgeHandleOptions";
+import { useContext, useEffect } from "react";
 import { NetworkContext } from "../../contexts/NetworkContext";
 import { CustomDeviceNode } from "./CustomDeviceNode";
 import { ToolBox } from "./ToolBox";
@@ -10,11 +12,13 @@ import { Modal } from "./Modals/Modal";
 import { BackgroundNode } from "./BackgroundNode";
 
 cytoscape.use(dagre);
+cytoscape.use(edgehandles);
 nodeHtmlLabel(cytoscape);
 
 export const NetworkMap = () => {
-  const { nodes, isModalOpen } = useContext(NetworkContext);
-  const cyRef = useRef(null);
+  const { cyRef, nodes, edges, isLinking, isModalOpen } =
+    useContext(NetworkContext);
+
   useEffect(() => {
     const cy = cytoscape({
       container: document.getElementById("cy"),
@@ -23,7 +27,7 @@ export const NetworkMap = () => {
         padding: 24,
         spacingFactor: 1.5,
       },
-      elements: nodes,
+      elements: [...nodes, ...edges],
       zoomingEnabled: true,
       userZoomingEnabled: true,
       autoungrabify: false,
@@ -58,6 +62,7 @@ export const NetworkMap = () => {
       },
     ]);
 
+    // eh.enableDrawMode();
     cyRef.current = cy;
 
     return () => {
@@ -69,8 +74,7 @@ export const NetworkMap = () => {
     if (cyRef.current) {
       const cy = cyRef.current;
       cy.elements().remove();
-      cy.add(nodes);
-
+      cy.add([...nodes, ...edges]);
       cy.layout({
         name: "dagre",
         padding: 24,
@@ -78,9 +82,57 @@ export const NetworkMap = () => {
       }).run();
     }
   }, [nodes]);
+  useEffect(() => {
+    const cy = cyRef.current;
+    const eh = cy.edgehandles(EHoptions);
+
+    if (isLinking) {
+      eh.enableDrawMode();
+
+      cy.on("ehstart", (event, sourceNode) => {
+        if (sourceNode.id() === "background") {
+          eh.stop();
+        }
+      });
+    } else {
+      eh.disableDrawMode();
+      cy.off("ehstart");
+    }
+
+    return () => {
+      eh.disableDrawMode();
+      cy.off("ehstart");
+    };
+  }, [isLinking]);
+
+  const infoButtonOnClick = () => {
+    // 노드 정보 가져오기
+    const cy = cyRef.current;
+    const nodesInfo = cy.nodes().map((node) => {
+      return {
+        id: node.id(),
+        position: node.position(),
+      };
+    });
+
+    // 엣지 정보 가져오기
+    const edgesInfo = cy.edges().map((edge) => {
+      return {
+        id: edge.id(),
+        source: edge.source().id(),
+        target: edge.target().id(),
+        position: edge.position(), // 엣지는 위치가 없으므로 이 부분은 필요에 따라 수정
+      };
+    });
+
+    // 콘솔에 정보 출력
+    console.log("노드 정보:", nodesInfo);
+    console.log("엣지 정보:", edgesInfo);
+  };
 
   return (
     <>
+      <button onClick={infoButtonOnClick}>정보 출력</button>
       {isModalOpen && <Modal />}
       <ToolBox />
       <div
