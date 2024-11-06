c4446a524aa7b47814778595dd2ec963adf791ab
cookooyas <cookooyas@gmail.com>
2024-11-07 01:46:50 +0900

    [SALT-32] Add: navigator와 관련된 config 파일 추가

---
+++ src/constants/NavigatorConfig.js
@@ -0,0 +1,10 @@
+let defaults = {
+  container: document.getElementById('navigator-container'),
+  viewLiveFramerate: 0,
+  thumbnailEventFramerate: 30,
+  thumbnailLiveFramerate: false,
+  dblClickDelay: 200,
+  removeCustomContainer: true,
+  rerenderDelay: 100,
+};
+export default defaults;
