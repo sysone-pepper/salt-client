aaa4676c95657c03f6432a27b7c1f3f94154e930
cookooyas <cookooyas@gmail.com>
2024-11-08 05:09:07 +0900

    [SALT-64] Feat: 배경이미지 수정 및 초기 화면 로드 시, 최적화 로직 구현. 배경 이미지 초기 로드시, 또는 업데이트 시  중앙에 오고 화면을 채울 수 있도록 기능 추가.

--- src/constants/NavigatorConfig.js
+++ src/constants/NavigatorConfig.js
@@ -1,10 +1,18 @@
 let defaults = {
-  container: document.getElementById('navigator-container'),
-  viewLiveFramerate: 0,
-  thumbnailEventFramerate: 30,
-  thumbnailLiveFramerate: false,
-  dblClickDelay: 200,
-  removeCustomContainer: true,
-  rerenderDelay: 100,
+  // container: document.getElementById('navigator-container'),
+  // viewLiveFramerate: 0,
+  // thumbnailEventFramerate: 30,
+  // thumbnailLiveFramerate: false,
+  // dblClickDelay: 200,
+  // removeCustomContainer: true,
+  // rerenderDelay: 100,
+
+  container: false, // string | false | undefined. Supported strings: an element id selector (like "#someId"), or a className selector (like ".someClassName"). Otherwise an element will be created by the library.
+  viewLiveFramerate: 0, // set false to update graph pan only on drag end; set 0 to do it instantly; set a number (frames per second) to update not more than N times per second
+  thumbnailEventFramerate: 30, // max thumbnail's updates per second triggered by graph updates
+  thumbnailLiveFramerate: false, // max thumbnail's updates per second. Set false to disable
+  dblClickDelay: 200, // milliseconds
+  removeCustomContainer: true, // destroy the container specified by user on plugin destroy
+  rerenderDelay: 100, // ms to throttle rerender updates to the panzoom for performance
 };
 export default defaults;
