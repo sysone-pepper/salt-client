81124154642e691aaabd2133c0629f5c6a15b196
Sohottoday <human9062@naver.com>
2024-11-08 17:40:33 +0900

    [SALT-66] Fix: 뒤로가기 관련 인증 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import AnimatedBackground from './layout/AnimatedBackground';
+import PrivateRoute from './components/common/PrivateRoute';
 import ProjectPage from './pages/ProjectPage';
 import ServerDashboard from './pages/ServerDashboard';
 
@@ -19,14 +20,36 @@ function App() {
                 <Route index element={<LoginPage />} />
                 <Route
                   path="projects/:username"
-                  element={<ProjectDashboardPage />}
+                  element={
+                    <PrivateRoute>
+                      <ProjectDashboardPage />
+                    </PrivateRoute>
+                  }
                 />
               </Route>
-              <Route path="project/" element={<ProjectPage />} />
-              <Route path="dashboard/" element={<ServerDashboard />} />
+              <Route
+                path="project/"
+                element={
+                  <PrivateRoute>
+                    <ProjectPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="dashboard/"
+                element={
+                  <PrivateRoute>
+                    <ServerDashboard />
+                  </PrivateRoute>
+                }
+              />
               <Route
                 path="project-detail/:projectId"
-                element={<ProjectPage />}
+                element={
+                  <PrivateRoute>
+                    <ProjectPage />
+                  </PrivateRoute>
+                }
               />
             </Routes>
           </Suspense>
@@ -35,5 +58,33 @@ function App() {
     </Router>
   );
 }
+
+// function App() {
+//   return (
+//     <Router>
+//       <AuthProvider>
+//         <div className="App">
+//           <Suspense fallback={<div>Loading...</div>}>
+//             <Routes>
+//               <Route path="/" element={<AnimatedBackground />}>
+//                 <Route index element={<LoginPage />} />
+//                 <Route
+//                   path="projects/:username"
+//                   element={<ProjectDashboardPage />}
+//                 />
+//               </Route>
+//               <Route path="project/" element={<ProjectPage />} />
+//               <Route path="dashboard/" element={<ServerDashboard />} />
+//               <Route
+//                 path="project-detail/:projectId"
+//                 element={<ProjectPage />}
+//               />
+//             </Routes>
+//           </Suspense>
+//         </div>
+//       </AuthProvider>
+//     </Router>
+//   );
+// }
 
 export default App;
