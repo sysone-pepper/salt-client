c6e75cd8ab9ef568ed929412f69bb921c569176d
cookooyas <cookooyas@gmail.com>
2024-11-13 08:28:30 +0900

    [SALT-90] Style: 코드 컨벤션

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -4,9 +4,9 @@
 마치 호텔 직원이 고객의 신분증을 확인하는 것과 같은 역할
 */
 
-import api from "./index";
+import api from './index';
 
 export const loginUser = async (credentials) => {
-  const response = await api.post("/login", credentials);
+  const response = await api.post('/login', credentials);
   return response.data;
 };
