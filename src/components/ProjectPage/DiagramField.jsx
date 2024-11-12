8720333ecc20a9996e53dcf2e9d491ad620ad93b
cookooyas <cookooyas@gmail.com>
2024-11-12 13:21:42 +0900

    [SALT-84] Fix: 텍스트 노드가 cy에 반영되도록 기능 개선

--- src/components/ProjectPage/DiagramField.jsx
+++ src/components/ProjectPage/DiagramField.jsx
@@ -13,6 +13,7 @@ import ehConfig from '../../constants/EdgeHandleOptions';
 import { CustomDeviceNode } from './CustomDeviceNode';
 import { CustomIconNode } from './CustomIconNode';
 import Sidebar from './Sidebar/Sidebar';
+import { CustomTextNode } from './CustomTextNode';
 
 const registerCytoscapeExtensions = () => {
   cytoscape.use(dagre);
@@ -140,8 +141,20 @@ export const DiagramField = ({ projectId }) => {
             )}`;
           },
         },
+        {
+          query: '.TEXT',
+          halign: 'center',
+          valign: 'center',
+          halignBox: 'center',
+          valignBox: 'center',
+          cssClass: '',
+          tpl(data) {
+            return `${ReactDOMServer.renderToString(
+              <CustomTextNode node={cy.getElementById(data.id)} data={data} />,
+            )}`;
+          },
+        },
       ]);
-
       cy.on('dragfree', 'node', async (event) => {
         const target = event.target;
         const updatedNode = target.json();
