afec60f455206b1e1daa9eab82c0bbb4dd1bbaf3
cookooyas <cookooyas@gmail.com>
2024-11-24 06:24:21 +0900

    [SALT-131] Chore: API 서버 환경변수화

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,7 @@
-/*
-서버와 통신할 때 필요한 기본 설정
-로그인 토큰을 자동으로 요청에 포함
-마치 호텔의 통신 시스템 설정같은 역할
-*/
-
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL, //|| 'http://localhost:8080',
 });
 
 api.interceptors.request.use(
@@ -24,11 +18,8 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response) => response, // 정상 응답은 그대로 반환
   (error) => {
-    console.log(error.response.status);
-    console.log(error.response.status === 400);
     if (error.response) {
       if (error.response.status === 400 || error.response.status === 401) {
-        console.log('flag1');
         alert('유효하지 않은 요청입니다.');
         localStorage.removeItem('token');
         localStorage.removeItem('currentUser');
